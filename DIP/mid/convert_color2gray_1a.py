c1bbbcec9eb83e2e42b9d92ef04d49aa42fe8719
Sorongos <dosora9@gmail.com>
2024-11-24 03:02:37 +0900

    1a: raw 이미지 불러오기

---
+++ DIP/mid/convert_color2gray_1a.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+width = 600
+height = 600
+channels = 1
+
+file_path = 'images/desk.raw'
+
+image_data = np.fromfile(file_path, dtype=np.uint8)
+print(image_data.shape)
+image = image_data.reshape((height, width, channels))
+if image is None: raise Exception("영상파일 읽기 오류")
+
+cv2.imshow("image", image)
+cv2.waitKey(0)
