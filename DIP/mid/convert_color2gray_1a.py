de147027719fc9940ef46ac196b91077cbcf49ce
Sorongos <dosora9@gmail.com>
2024-11-27 00:36:00 +0900

    mid: 1a width, height 지정

--- DIP/mid/convert_color2gray_1a.py
+++ DIP/mid/convert_color2gray_1a.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-width = 600
-height = 600
-channels = 1
+width = 400
+height = 300
+channels = 3
 
 file_path = 'images/desk.raw'
 
-image_data = np.fromfile(file_path, dtype = np.uint8)
+image_data = np.fromfile(file_path, dtype=np.uint8)
 print(image_data.shape)
 image = image_data.reshape((height, width, channels))
 if image is None: raise Exception("영상파일 읽기 오류")
