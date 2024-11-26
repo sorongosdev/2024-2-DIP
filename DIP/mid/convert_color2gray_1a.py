c3074a1b4c6d32bf7f336e3be657218663f543c7
sorongosdev <dosora9@gmail.com>
2024-11-26 10:13:34 +0900

    mid: 조교님 질문

--- DIP/mid/convert_color2gray_1a.py
+++ DIP/mid/convert_color2gray_1a.py
@@ -7,10 +7,12 @@ channels = 1
 
 file_path = 'images/desk.raw'
 
-image_data = np.fromfile(file_path, dtype=np.uint8)
+image_data = np.fromfile(file_path, dtype = np.uint8)
 print(image_data.shape)
 image = image_data.reshape((height, width, channels))
 if image is None: raise Exception("영상파일 읽기 오류")
 
 cv2.imshow("image", image)
 cv2.waitKey(0)
+
+## 바이트 설정?? 찾아볼것.
