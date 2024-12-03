283b75f90dbca20506a72fb5ed974d44127ddc7c
sorongosdev <dosora9@gmail.com>
2024-12-03 10:32:06 +0900

    ch12_hw: 타원방정식으로 변경

--- DIP/ch12_hw/hw_hough_circle.py
+++ DIP/ch12_hw/hw_hough_circle.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 
 image = cv2.imread("images/dish.png", cv2.IMREAD_COLOR)
@@ -7,26 +6,19 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 gray_blurred = cv2.GaussianBlur(gray, (9, 9), 2)
 
-# 가우시안 블러로 노이즈 제거
-circles = cv2.HoughCircles(gray_blurred,
-                           cv2.HOUGH_GRADIENT,
-                           1,
-                           gray_blurred.shape[0] / 8,
-                           200,
-                           param2 = 50,
-                           minRadius = 0,
-                           maxRadius = 0)
+edges = cv2.Canny(gray_blurred, 50, 150)
+
+contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 output = image.copy()
-if circles is not None:
-    circles = np.uint16(np.around(circles))
-    for circle in circles[0, :]:
-        x, y, radius = circle
-        cv2.circle(output, (x, y), radius, (0, 255, 0), 2)
-        cv2.circle(output, (x, y), 2, (0, 0, 255), 3)
+
+for contour in contours:
+    if len(contour) >= 5:  # 타원을 맞추기 위해 최소 5개의 점이 필요
+        ellipse = cv2.fitEllipse(contour)
+        cv2.ellipse(output, ellipse, (0, 255, 0), 2)
 
 plt.figure(figsize = (10, 10))
 plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-plt.title("Circular Hough Transform")
+plt.title("Elliptical Hough Transform")
 plt.axis('off')
 plt.show()
